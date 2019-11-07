7f1579f984118d214e2bcdd8af45f6916fa47328
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-11-07 19:30:36 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan49-EmasKawin/src/if1/pkg10118042/latihan49/emaskawin/Emas.java
@@ -0,0 +1,30 @@
+
+package if1.pkg10118042.latihan49.emaskawin;
+
+
+public class Emas {
+  public int hargaGram;
+    public double beratEmas;
+
+    public int getHargaGram() {
+        return hargaGram;
+    }
+
+    public void setHargaGram(int hargaGram) {
+        this.hargaGram = hargaGram;
+    }
+
+    public double getBeratEmas() {
+        return beratEmas;
+    }
+
+    public void setBeratEmas(double beratEmas) {
+        this.beratEmas = beratEmas;
+    }
+   
+   public double perhitungan(int hargaGram, double beratEmas) {
+       
+       return hargaGram * beratEmas ;
+    }
+    
+}
---
+++ IF1-10118042-Latihan49-EmasKawin/src/if1/pkg10118042/latihan49/emaskawin/IF110118042Latihan49EmasKawin.java
@@ -0,0 +1,37 @@
+
+package if1.pkg10118042.latihan49.emaskawin;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini berfungsi untuk menghitung Harga Emas
+ */
+public class IF110118042Latihan49EmasKawin {
+
+    public static void main(String[] args) {
+     DecimalFormat mataUangIndonesia = (DecimalFormat) 
+                      DecimalFormat.getCurrencyInstance();
+        DecimalFormatSymbols formatRP = new DecimalFormatSymbols();
+        
+        formatRP.setCurrencySymbol("Rp.");
+        formatRP.setMonetaryDecimalSeparator(',');
+        formatRP.setGroupingSeparator('.');
+        
+        mataUangIndonesia.setDecimalFormatSymbols(formatRP);
+        
+        
+        Emas objEmas = new Emas();
+        objEmas.setHargaGram(570000);
+        objEmas.setBeratEmas(15.7);
+        System.out.println("====Aplikasi Hitung Harga Emas====");
+        System.out.printf("Total Bayar Emas : %s %n ", 
+                           mataUangIndonesia.format(
+                           objEmas.perhitungan(objEmas.getHargaGram(), 
+                           objEmas.getBeratEmas())));
+    }
+    
+}
